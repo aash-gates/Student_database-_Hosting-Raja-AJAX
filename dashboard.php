ff696a3b80cc734933fe990ec4fadc84928b7a8b
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2024-03-31 21:22:58 +0530

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -137,6 +137,7 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
                                         <th>Student ID</th>
                                     </tr>
                                 </thead>
+                                <tbody id="student-table-body">
                                     <?php
                                     // Include db_connect.php to establish a database connection
                                     include 'db_connect.php';
