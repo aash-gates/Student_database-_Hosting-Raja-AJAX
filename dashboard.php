b6f2d32170cef2bf0610f575cf421a496d072858
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2024-03-31 21:23:54 +0530

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -223,6 +223,7 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
             $.ajax({
                 url: 'get_students.php?page=' + page,
                 success: function(response) {
+                    // Hide loading spinner
         // Initial call to fetch time and quote information
         getTime();
         getQuote();
