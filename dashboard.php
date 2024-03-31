f940d4fc82be75ed0692cb266268286e0dc8acf3
Aashik J Krishnan <58915216+aash-gates@users.noreply.github.com>
2024-03-31 21:39:24 +0530

    Update dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -245,6 +245,7 @@ if (!isset($_SESSION['loggedin']) || $_SESSION['loggedin'] !== true) {
             e.preventDefault();
             var page = $(this).attr('href').split('page=')[1];
             getStudents(page);
+        });
     </script>
 </body>
 
